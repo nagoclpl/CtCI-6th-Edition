914900a285342daaff8553f2f73dd7cd70c57cd4
Naga <nvijayap@gmail.com>
2019-03-19 23:29:47 -0700

    QuestionA.scala

---
+++ Scala/Ch_01._Arrays_and_Strings/Q1_01_Is_Unique/QuestionA.scala
@@ -0,0 +1,33 @@
+/**
+ * Question A
+ */
+object QuestionA extends App {
+
+  /**
+   * Does str have unique chars in it?
+   */
+  def isUniqueChars(str: String): Boolean = {
+
+    if (str.size > 128) {
+      return false
+    }
+
+    val char_set = new Array[Boolean](128)
+
+    for (i <- 0 to str.size-1) {
+      val v = str.charAt(i)
+      if (char_set(v)) return false
+      char_set(v) = true
+    }
+
+    true
+  }
+
+  /**
+   * The execution starts here
+   */
+
+  val words = List("abcde", "hello", "apple", "kite", "padle")
+
+  words.foreach(word => printf("%9s: %-9s\n", word, isUniqueChars(word)))
+}
